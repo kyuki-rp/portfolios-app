aab574ab699a7225196dd2fde172a69ee44956b7
k-yukio <k-yuki@analytics-jp.com>
2021-09-05 21:31:47 +0900

    yuki_work_20210905

--- build_tools/build_ecs/main.tf
+++ build_tools/build_ecs/main.tf
@@ -3,24 +3,24 @@ variable "app_name" {}
     
 module "network" {
   source = "./network"
-  app_name = "${var.app_name}"
+  app_name = var.app_name
 }
 
 ## Cluster
 resource "aws_ecs_cluster" "default" {
-  name = "${var.app_name}"
+  name = var.app_name
 }
  
 ## Task
 resource "aws_ecs_task_definition" "default" {
-  family                             =  "${var.app_name}"
+  family                             = var.app_name
   requires_compatibilities           = ["FARGATE"]
   network_mode                       = "awsvpc"
   task_role_arn                      = "arn:aws:iam::${var.aws_account_id}:role/ecsExecRole"
   execution_role_arn                 = "arn:aws:iam::${var.aws_account_id}:role/ecsTaskExecutionRole"
   cpu                                = 512
   memory                             = 1024
-  container_definitions              = templatefile("./container_definitions.json", {aws_account_id = "${var.aws_account_id}"})
+  container_definitions              = templatefile("./container_definitions.json", {aws_account_id = var.aws_account_id})
 }
 
 ## Service
@@ -30,7 +30,7 @@ resource "aws_ecs_service" "default" {
   deployment_maximum_percent         = 200
   desired_count                      = 1
   launch_type                        = "FARGATE"
-  name                               = "${var.app_name}"
+  name                               = var.app_name
  
   network_configuration {
     subnets = [module.network.aws_subnet.id]
