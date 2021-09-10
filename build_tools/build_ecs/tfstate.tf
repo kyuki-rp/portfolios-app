09606202bf319df44129ae29c98674a03a0849ae
k-yukio <k-yuki@analytics-jp.com>
2021-09-10 22:03:57 +0900

    yuki_work_20210910

--- build_tools/build_ecs/tfstate.tf
+++ build_tools/build_ecs/tfstate.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 0.11.0"
-  backend "s3" {
-    bucket = "tfstate-build-ecs"
-    region = "us-east-1"
-    key = "terraform.tfstate"
-    encrypt = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -18,3 +8,14 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 }
+
+terraform {
+  required_version = ">= 0.11.0"
+  backend "s3" {
+    bucket = "tfstate-build-ecs"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
