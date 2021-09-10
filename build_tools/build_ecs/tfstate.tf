0f6372931e7f5cd137a0bfcf6fbf116430f409c4
k-yukio <k-yuki@analytics-jp.com>
2021-09-10 22:24:08 +0900

    yuki_work_20210910

--- build_tools/build_ecs/tfstate.tf
+++ build_tools/build_ecs/tfstate.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   required_version = ">= 0.11.0"
   backend "s3" {
-    bucket = "tfstate-build-ecs"
+    bucket = "tfstate"
     region = "ap-northeast-1"
     key = "tf_build_ecs.tfstate"
     encrypt = true
--- build_tools/push_ecr/tfstate.tf
+++ build_tools/push_ecr/tfstate.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   required_version = ">= 0.11.0"
   backend "s3" {
-    bucket = "tfstate-push-ecr"
+    bucket = "tfstate"
     region = "ap-northeast-1"
     key = "tf_push_ecr.tfstate"
     encrypt = true
