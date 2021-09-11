9050798e53214a87310b3cf4a7d79fbc30080d07
k-yukio <k-yuki@analytics-jp.com>
2021-09-11 13:29:28 +0900

    20210911

--- build_tools/push_ecr/network/main.tf
+++ build_tools/push_ecr/network/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "default" {
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = "true"
-  tags = {Name = var.app_name}
+  tags = {"Name" = var.app_name}
 }
 
 module "igw" {
