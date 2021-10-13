2b86abe975b077212836fb55207ce814bc090b4e
k-yukio <k-yuki@analytics-jp.com>
2021-10-13 15:27:19 +0900

    20211013_yuki

--- build_tools/build_ecs/main.tf
+++ build_tools/build_ecs/main.tf
@@ -19,7 +19,7 @@ resource "aws_ecs_cluster" "default" {
 }
  
 ## Task
-resource "aws_ecs_task_definition" "default" {
+resource "aws_ecs_task_definition" "traefik" {
   family                             = var.app_name
   requires_compatibilities           = ["FARGATE"]
   network_mode                       = "awsvpc"
@@ -27,11 +27,22 @@ resource "aws_ecs_task_definition" "default" {
   execution_role_arn                 = "arn:aws:iam::${var.aws_account_id}:role/ecsTaskExecutionRole"
   cpu                                = 256
   memory                             = 512
-  container_definitions              = templatefile("./container_definitions.json", {aws_account_id = var.aws_account_id, domain_name = var.domain_name})
+  container_definitions              = templatefile("./container_definitions_traefik.json", {aws_account_id = var.aws_account_id, domain_name = var.domain_name})
+}
+
+resource "aws_ecs_task_definition" "whoami" {
+  family                             = var.app_name
+  requires_compatibilities           = ["FARGATE"]
+  network_mode                       = "awsvpc"
+  task_role_arn                      = "arn:aws:iam::${var.aws_account_id}:role/ecsExecRole"
+  execution_role_arn                 = "arn:aws:iam::${var.aws_account_id}:role/ecsTaskExecutionRole"
+  cpu                                = 256
+  memory                             = 512
+  container_definitions              = templatefile("./container_definitions_whoami.json", {aws_account_id = var.aws_account_id, domain_name = var.domain_name})
 }
 
 ## Service
-resource "aws_ecs_service" "default" {
+resource "aws_ecs_service" "traefik" {
   cluster                            = aws_ecs_cluster.default.id
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
@@ -55,7 +66,37 @@ resource "aws_ecs_service" "default" {
     weight            = 0
   }
  
-  task_definition = aws_ecs_task_definition.default.arn
+  task_definition = aws_ecs_task_definition.traefik.arn
+
+  enable_execute_command = "true"
+}
+
+## Service
+resource "aws_ecs_service" "whoami" {
+  cluster                            = aws_ecs_cluster.default.id
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  desired_count                      = 1
+#  launch_type                        = "FARGATE"
+  name                               = var.app_name
+ 
+  network_configuration {
+    assign_public_ip = "true"
+    subnets = [module.network.aws_subnet_id]
+    security_groups = [module.network.aws_security_group_id]
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 100
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 0
+  }
+ 
+  task_definition = aws_ecs_task_definition.whoami.arn
 
   enable_execute_command = "true"
 }
