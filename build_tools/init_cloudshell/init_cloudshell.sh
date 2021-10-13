71c44f99a7d2d917de2d6301aca5024c75fcc100
k-yukio <k-yuki@analytics-jp.com>
2021-10-13 10:32:45 +0900

    20211013

--- build_tools/init_cloudshell/init_cloudshell.sh
+++ build_tools/init_cloudshell/init_cloudshell.sh
@@ -10,7 +10,9 @@ UserId=`aws sts get-caller-identity | jq '.UserId'`
 Account=`aws sts get-caller-identity | jq '.Account'`
 Arn=`aws sts get-caller-identity | jq '.Arn'`
 S3Bucket='"tfstate-u5n1k2x1"'
-HostZoneId=`aws route53 list-hosted-zones --output json | jq -r ".HostedZones[] | select(.Name == /"$DomainName./") | .Id"`
+DomainName1=\"${DomainName}.\"
+DomainName2=\"${DomainName}\"
+HostZoneId=`aws route53 list-hosted-zones --output json | jq -r ".HostedZones[] | select(.Name == $DomainName1) | .Id"`
 HostZoneId=\"${HostZoneId#\/hostedzone\/}\"
 rm vars.tfvars -f
 touch vars.tfvars
@@ -19,6 +21,7 @@ echo "aws_account_id=$Account" >> vars.tfvars
 echo "aws_arn=$Arn" >> vars.tfvars
 echo "tfstate_s3bucketname=$S3Bucket" >> vars.tfvars
 echo "hostzone_id=$HostZoneId" >> vars.tfvars
+echo "domain_name=$DomainName2" >> vars.tfvars
 
 # tfbackendの設定
 grep -lr --include="backend_config.tfbackend" "" ./* | xargs sed -i.bak -e "s/BucketName/$S3Bucket/g"
