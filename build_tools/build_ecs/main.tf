8f5836a05deb7f2e66b9464f1201dc6512c149d4
k-yukio <k-yuki@analytics-jp.com>
2021-10-13 16:28:58 +0900

    20211013_yuki

--- build_tools/build_ecs/main.tf
+++ build_tools/build_ecs/main.tf
@@ -20,7 +20,7 @@ resource "aws_ecs_cluster" "default" {
  
 ## Task
 resource "aws_ecs_task_definition" "traefik" {
-  family                             = var.app_name
+  family                             = "traefik"
   requires_compatibilities           = ["FARGATE"]
   network_mode                       = "awsvpc"
   task_role_arn                      = "arn:aws:iam::${var.aws_account_id}:role/ecsExecRole"
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "traefik" {
 }
 
 resource "aws_ecs_task_definition" "whoami" {
-  family                             = var.app_name
+  family                             = "whoami"
   requires_compatibilities           = ["FARGATE"]
   network_mode                       = "awsvpc"
   task_role_arn                      = "arn:aws:iam::${var.aws_account_id}:role/ecsExecRole"
@@ -47,7 +47,7 @@ resource "aws_ecs_service" "traefik" {
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   desired_count                      = 1
-#  launch_type                        = "FARGATE"
+#  launch_type                       = "FARGATE"
   name                               = var.app_name
  
   network_configuration {
@@ -71,32 +71,3 @@ resource "aws_ecs_service" "traefik" {
   enable_execute_command = "true"
 }
 
-## Service
-resource "aws_ecs_service" "whoami" {
-  cluster                            = aws_ecs_cluster.default.id
-  deployment_minimum_healthy_percent = 100
-  deployment_maximum_percent         = 200
-  desired_count                      = 1
-#  launch_type                        = "FARGATE"
-  name                               = var.app_name
- 
-  network_configuration {
-    assign_public_ip = "true"
-    subnets = [module.network.aws_subnet_id]
-    security_groups = [module.network.aws_security_group_id]
-  }
-
-  capacity_provider_strategy {
-    capacity_provider = "FARGATE_SPOT"
-    weight            = 100
-  }
-
-  capacity_provider_strategy {
-    capacity_provider = "FARGATE"
-    weight            = 0
-  }
- 
-  task_definition = aws_ecs_task_definition.whoami.arn
-
-  enable_execute_command = "true"
-}
