8391b423c9454edee53f546f0c26c5a9b318941a
k-yukio <k-yuki@analytics-jp.com>
2021-11-05 15:59:55 +0900

    20211105_yuki_work

--- middenii/docker/react/src/App.tsx
+++ middenii/docker/react/src/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import ViewAll from "./view_all";  // 追加することで<Sample />が利用できる
 import ViewOne from "./view_one";  // 追加することで<Sample />が利用できる
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <>
       <h1>今日の星座占い</h1>
-      <ViewOne />
-      <ViewAll />
+      <div className="card">
+        <div className="card-body">
+          <p>bootstrap</p>
+          <Button variant="outline-primary">プライマリーボタン</Button>
+        </div>
+      </div>
     </>
   );
 }
--- middenii/docker/react/src/index.tsx
+++ middenii/docker/react/src/index.tsx
@@ -2,13 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <div>
-     Hello World!!! TEST.
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
