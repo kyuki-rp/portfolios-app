8ce5bf5ee05005db50a4c1a9d6b547847d810fed
k-yukio <k-yuki@analytics-jp.com>
2021-11-05 19:46:58 +0900

    20211105_yuki_work

--- middenii/docker/react/src/main.tsx
+++ middenii/docker/react/src/main.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { SampleComponent } from './sample-component';
-import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
   render() {
@@ -9,11 +8,6 @@ class App extends React.Component {
       <div>
         <h1>React Example</h1>
         <SampleComponent name="Counter"/>
-        <div className="card">
-          <div className="card-body">
-            <Button variant="outline-primary">プライマリーボタン</Button>
-          </div>
-        </div>
       </div>
     );
   }
