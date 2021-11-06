20c165f8f547197fe941459203d05d4b7d19b1e8
k-yukio <k-yuki@analytics-jp.com>
2021-11-06 14:39:07 +0900

    20211106_yuki_work

--- middenii/docker/react/src/main.tsx
+++ middenii/docker/react/src/main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { SampleComponent } from './sample-component';
 import ViewAll from "./view_all";  // 追加することで<Sample />が利用できる
-import ViewOne from "./view_one";  // 追加することで<Sample />が利用できる
 
 class App extends React.Component {
   render() {
