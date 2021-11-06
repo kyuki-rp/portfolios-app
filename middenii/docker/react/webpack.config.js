f84800279bc20c684ddaec7d7d8069b752169c67
k-yukio <k-yuki@analytics-jp.com>
2021-11-06 17:05:09 +0900

    20211106

--- middenii/docker/react/webpack.config.js
+++ middenii/docker/react/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       directory: path.join(__dirname, "dist"),
     },
     historyApiFallback: true,
-    allowedHosts: 'auto',
+    allowedHosts: 'all',
     port: 3000
 	}
 };
