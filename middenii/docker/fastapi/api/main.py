76df708cc116f4e7da3895c56e393b0c1e06c2c7
k-yukio <k-yuki@analytics-jp.com>
2021-11-09 20:58:58 +0900

    20211109

--- middenii/docker/fastapi/api/main.py
+++ middenii/docker/fastapi/api/main.py
@@ -5,15 +5,10 @@ import pandas as pd
 import datetime
 from starlette.middleware.cors import CORSMiddleware
 
-origins = [
-    "https://works.middenii.com",
-    "http://works.middenii.com:3000"
-]
-
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
