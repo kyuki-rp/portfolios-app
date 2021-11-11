6ce364917b7c4c64e51cbcefbafd2d297732b3cf
k-yukio <k-yuki@analytics-jp.com>
2021-11-11 12:57:03 +0900

    20211111

--- middenii/docker/zodiac/react/src/index.tsx
+++ middenii/docker/zodiac/react/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './main';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import createBrowserHistory from 'history/createBrowserHistory';
-                                                                                const history = createBrowserHistory({ basename: '/frontend' }); // 追加
+// import createBrowserHistory from 'history/createBrowserHistory';
+// const history = createBrowserHistory({ basename: '/frontend' }); // 追加
+
 ReactDOM.render(
   <React.StrictMode>
-    <ConnectedRouter history={history}>
+    {/* <ConnectedRouter history={history}> */}
       <App />
-    </ConnectedRouter>
+    {/* </ConnectedRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
