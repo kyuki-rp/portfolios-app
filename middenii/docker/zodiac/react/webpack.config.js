b800932d88657f434f06c191b324e47e8a296341
k-yukio <k-yuki@analytics-jp.com>
2021-11-14 14:36:59 +0900

    20211114

--- middenii/docker/zodiac/react/webpack.config.js
+++ middenii/docker/zodiac/react/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: "index.js",
     path: path.resolve("dist"),
     // publicPath: "/frontend"
-    publicPath: "/"
+    publicPath: "/frontend"
   },
   module: {
     rules: [
