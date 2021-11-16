1644040274f9955f5d33f9944fe8dd5aecdc1af4
k-yukio <k-yuki@analytics-jp.com>
2021-11-16 17:06:48 +0900

    20211116

--- middenii/userdata.sh
+++ middenii/userdata.sh
@@ -31,3 +31,10 @@ cd /home/ec2-user/cloudtools/middenii/docker/zodiac
 docker-compose build
 docker-compose run frontend sh -c "npm init -y && npm install -D webpack webpack-cli webpack-dev-server typescript ts-loader @types/react @types/react-dom react-router-dom @types/react-router-dom axios bootstrap css-loader style-loader react-bootstrap history@4.10.1 && npm install react react-dom"
 docker-compose up -d
+
+# zodiac_copy app docker-compose up
+cd /home/ec2-user/cloudtools/middenii/docker/zodiac_copy
+docker-compose build
+docker-compose run frontend sh -c "npm init -y && npm install -D webpack webpack-cli webpack-dev-server typescript ts-loader @types/react @types/react-dom react-router-dom @types/react-router-dom axios bootstrap css-loader style-loader react-bootstrap history@4.10.1 && npm install react react-dom"
+docker-compose up -d
+
