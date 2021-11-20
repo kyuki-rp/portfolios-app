b4718823fd79b867f60db74461772414fc429ca4
k-yukio <k-yuki@analytics-jp.com>
2021-11-20 20:44:27 +0900

    202111120

--- middenii/userdata.sh
+++ middenii/userdata.sh
@@ -37,7 +37,9 @@ docker network create web
 # export app_name="zodiac2"
 # docker-compose --compatibility up --build -d
 
-for dir_path in /home/ec2-user/cloudtools/middenii/docker/*; do echo "$file"; done
+for dir_path in /home/ec2-user/cloudtools/middenii/docker/*; do
+ echo $dir_path
 #   cd "/home/ec2-user/cloudtools/middenii/docker/${file##*/}"
 #   export app_name="$\{file##*/\}"
 #   docker-compose --compatibility up --build -d
+done
